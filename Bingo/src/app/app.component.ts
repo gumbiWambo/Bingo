d6bbe760c4abe3c027c13bf334aab209ee1f8ebe
fibronachos <fibronachso@gmx.net>
2020-05-15 21:01:43 +0200

    docker stuff

--- Bingo/src/app/app.component.ts
+++ Bingo/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'b-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Bingo';
+export class AppComponent implements OnInit{
+  public title = 'Bingo';
+  public serverAnswer:string;
+
+  constructor(private http:HttpClient){}
+
+  public ngOnInit(){
+    this.http.get<string>('http://localhost:9100')
+    .toPromise()
+    .then(value => this.serverAnswer = value);
+  }
 }
